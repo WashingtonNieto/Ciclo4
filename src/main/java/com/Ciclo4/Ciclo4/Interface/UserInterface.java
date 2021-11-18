1d7872f6735811519255c30eff9d635de4f8b6bf
Wnieto <washington_nieto@hotmail.com>
2021-11-18 13:44:36 -0500

    incluye interface

---
+++ src/main/java/com/Ciclo4/Ciclo4/Interface/UserInterface.java
@@ -0,0 +1,7 @@
+package com.Ciclo4.Ciclo4.Interface;
+
+import com.Ciclo4.Ciclo4.entity.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserInterface extends CrudRepository<User, Integer> {
+}
